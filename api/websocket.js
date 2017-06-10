4c1359934945720d61a69a3e4023f396de9e4b38
Ricky Tan <422563261@qq.com>
2017-06-10 20:18:05 +0800

    beta

--- api/websocket.js
+++ api/websocket.js
@@ -1,116 +1,119 @@
-var {stdRes, Room} = require('../lib');
-var cookieParser = require('cookie-parser');
-var Cookie = require('cookie');
-var fs = require('fs');
-
+var {stdRes, Room} = require('../lib')
+var cookieParser = require('cookie-parser')
+var Cookie = require('cookie')
+var fs = require('fs')
 
 // 请求建立连接
 exports.connect = function (req, res) {
-  var token = req.params.token;
-  var passwd = req.params.passwd;
+  var token = req.params.token
+  var passwd = req.params.passwd
   if (!global.ROOMS[token]) {
-    res.send(stdRes('token无效', 102));
-    return;
+    res.send(stdRes('token无效', 102))
+    return
   }
   // 校验passwd
   if (global.ROOMS[token].passwd && global.ROOMS[token].passwd !== passwd) {
-    res.send(stdRes('password不正确', 104));
-    return;
+    res.send(stdRes('password不正确', 104))
+    return
   }
   // 通过
-  res.cookie('token', token);
-  if (passwd) res.cookie('passwd', passwd);
-  res.send(stdRes());
-  return;
-};
+  res.cookie('token', token)
+  if (passwd) res.cookie('passwd', passwd)
+  res.send(stdRes())
+  return
+}
 
 // 查看与websocket连接相关信息
 exports.getRooms = function (req, res) {
-  var result = {rooms: []};
+  var result = {rooms: []}
   for (let token in global.ROOMS) {
     result.rooms.push({
       id: global.ROOMS[token].roomId,
       token: global.ROOMS[token].token,
       passwd: !!global.ROOMS[token].token,
-    });
+    })
   }
-  ;
-  res.send(stdRes('ok', 0, result));
-  return;
 
+  res.send(stdRes('ok', 0, result))
+  return
 
-  var room;
+  var room
   for (var i = 0; i < ROOMS.length; i++) {
     room = {
       id: ROOMS[i].id,
       token: ROOMS[i].token
     }
-    rooms.push(room);
+    rooms.push(room)
   }
-  console.log("rooms:" + rooms);
+  console.log('rooms:' + rooms)
   // 返回
-  res.send(rooms);
+  res.send(rooms)
 
-};
-
+}
 
 global.IO.on('connection', function (socket) {
 
   if (!socket.request.headers.cookie) {
     // console.log('cookie is empty');
-    return;
+    return
   }
-  var cookieObj = Cookie.parse(socket.request.headers.cookie);
+  var cookieObj = Cookie.parse(socket.request.headers.cookie)
 
-  var token = cookieObj.token;
-  var passwd = cookieObj.passwd;
-  var room;
+  var token = cookieObj.token
+  var passwd = cookieObj.passwd
+  var room
   // console.log('client connection use ' + token);
 
   // 第一个使用token的为房间所有者
   if (global.TOKENS.indexOf(token) !== -1) {
     // console.log('create room: ' + token);
     // 销毁token
-    global.TOKENS.splice(global.TOKENS.indexOf(token), 1);
+    global.TOKENS.splice(global.TOKENS.indexOf(token), 1)
     // 创建房间
     room = new Room({
       token,
       roomId: global.ROOMS_ID++
-    });
-    global.ROOMS[token] = room;
+    })
+    global.ROOMS[token] = room
 
     // 为房间设置密码
     if (global.TOKEN2PASSWD[token]) {
-      room.passwd = global.TOKEN2PASSWD[token];
-      delete global.TOKEN2PASSWD[token];
+      room.passwd = global.TOKEN2PASSWD[token]
+      delete global.TOKEN2PASSWD[token]
     }
     // 添加连接，绑定事件
-    room.connectPool.push(socket);
+    room.connectPool.push(socket)
     socket.on('message', function (data) {
-      room.historyData.push(arguments);
+      room.historyData.push(arguments)
       for (let link of room.connectPool.slice(1)) {
-        link.emit('message', ...arguments);
+        link.emit('message', ...arguments)
       }
-    });
+    })
     socket.on('video', function (data) {
-      room.historyVideo.push(arguments)
+      // console.log(room.connectPool.length)
       for (let link of room.connectPool) {
-        link.emit('video', data);
+        link.emit('video', data)
       }
-    });
+    })
     socket.on('chat', function (data) {
       room.historyChat.push(arguments)
       for (let link of room.connectPool) {
-        link.emit('chat', data);
+        link.emit('chat', data)
       }
     })
-    return;
+    return
   }
 
   socket.on('chat', function (data) {
     global.ROOMS[token].historyChat.push(arguments)
     for (let link of global.ROOMS[token].connectPool) {
-      link.emit('chat', data);
+      link.emit('chat', data)
+    }
+  })
+  socket.on('video', function (data) {
+    global.ROOMS[token].historyChat.push(arguments)
+    for (let link of global.ROOMS[token].connectPool) {
+      link.emit('video', data)
     }
   })
 
@@ -118,29 +121,32 @@ global.IO.on('connection', function (socket) {
 
   if (!token || !global.ROOMS[token]) {
     // console.log('token invalid: ' + token);
-    socket.disconnect();
-    return;
+    socket.disconnect()
+    return
   }
   // passwd无效
   if (global.ROOMS[token].passwd && global.ROOMS[token].passwd !== passwd) {
     // console.log('password illegal');
-    socket.disconnect();
-    return;
+    socket.disconnect()
+    return
   }
 
   // 观众连接
-  // console.log('add connect pool: ' + token);
-  global.ROOMS[token].connectPool.push(socket);
+  console.log('add connect pool: ' + token)
+  global.ROOMS[token].connectPool.push(socket)
+  global.ROOMS[token].connectPool.forEach(link => {
+    link.emit('refresh', 'refresh')
+  })
   // 将所有历史数据推过去
   global.ROOMS[token].historyData.forEach(function (data) {
-    socket.emit('message', ...data);
-  });
-  global.ROOMS[token].historyVideo.forEach(function (data) {
-    socket.emit('video', ...data);
-  });
+    socket.emit('message', ...data)
+  })
+  // global.ROOMS[token].historyVideo.forEach(function (data) {
+  //   socket.emit('video', ...data)
+  // })
   global.ROOMS[token].historyChat.forEach(function (data) {
-    socket.emit('chat', ...data);
-  });
-});
+    socket.emit('chat', ...data)
+  })
+})
 
 
--- app.js
+++ app.js
@@ -1,65 +1,69 @@
-var config = require('./config');
-var express = require('express');
-var sio		= require('socket.io');
-var cookieParser = require('cookie-parser');
-var app =global.APP =express();
+var config = require('./config')
+var express = require('express')
+var sio = require('socket.io')
+var cookieParser = require('cookie-parser')
+var fs = require('fs')
+var https = require('https')
+var app = global.APP = express()
 
+var options = {
+  key: fs.readFileSync('./path/private.pem'),
+  cert: fs.readFileSync('./path/file.crt')
+}
 
-
-
-app.use(cookieParser());
+app.use(cookieParser())
 //全局变量
 /**
  * 全部房间
  * token:Array<Room>
  * */
-global.ROOMS ={};
+global.ROOMS = {}
 /**
  * 创建后未被使用的Token
  * @type {String[]}
  * */
-global.TOKENS =[];
+global.TOKENS = []
 /**
  * 下一个使用的房间ID
  * @type {Number}
  * */
-global.ROOMS_ID =1;
+global.ROOMS_ID = 1
 /**
  * 为申请房间的令牌设定的密码
  * token:password
  * */
-global.TOKEN2PASSWD ={};
+global.TOKEN2PASSWD = {}
 
+var server = https.createServer(options, app)
 
-var server = app.listen(config.port, function () {
-	console.log('Server listening at http://%s:%s', config.host, config.port);
-});
+server.listen(config.port, function () {
+  console.log('Server listening at http://%s:%s', config.host, config.port)
+})
 
 // 当 http server 接收到 upgrade websocket 时就将请求转给 socket.io 处理
-global.IO = sio.listen(server);
+global.IO = sio.listen(server)
 // 直播间
 
+var apiRouter = require('./api_router')
 
-var apiRouter = require('./api_router');
-
-app.use(function(req ,res ,next){
-    res.header('Access-Control-Allow-Origin', req.headers.origin);
-    res.header(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'
-    );
-    res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    if (req.method == 'OPTIONS') {
-        res.send(200); //options快速响应
-    }
-    else {
-        next();
-    }
-});
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', req.headers.origin)
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Content-Length, Authorization, Accept, X-Requested-With'
+  )
+  res.header('Access-Control-Allow-Methods', 'PUT, POST, GET, DELETE, OPTIONS')
+  res.header('Access-Control-Allow-Credentials', 'true')
+  if (req.method == 'OPTIONS') {
+    res.send(200) //options快速响应
+  }
+  else {
+    next()
+  }
+})
 
 // route
-app.use('/', apiRouter);
-// app.use(express.static('./dist'))
+app.use('/', apiRouter)
+app.use(express.static('./dist'))
 
-module.exports = app;
+module.exports = app
